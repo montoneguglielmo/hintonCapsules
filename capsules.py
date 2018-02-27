e067b43747be7cb636f8213b888570df178a1260
guglielmomontone <montone.guglielmo@gmail.com>
2018-02-27 16:32:06 +0100

    first commit

---
+++ capsules.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class primaryCapsule(nn.Conv2d):
+
+    def __init__(self, in_channels, n_channels, dim_vector, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=False):
+        
+        out_channels = n_channels * dim_vector
+        super(primaryCapsule, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+
+        self.dim_vector = dim_vector
+        self.n_channels = n_channels
+        
+    def forward(self, input):
+        output = super(primaryCapsule, self).forward(input)
+        output = output.view([output.shape[0], self.n_channels, self.dim_vector, output.shape[-2], output.shape[-1]])
+        output = self.squash(output)
+        return output
+
+    @staticmethod
+    def squash(input, dim=2):
+        s_square_norm = torch.sum((input * input), dim=dim, keepdim=True)
+        output  = s_square_norm/(1. + s_square_norm) * input/torch.sqrt(s_square_norm)
+        return output
+        
+
+
+class digitCapsule(nn.Module):
+
+    def __init__(self, n_inp_capsules, dim_inp_capsules, n_out_capsules, dim_out_capsules, num_routing=3):
+        super(digitCapsule,self).__init__()
+        self.n_inp_capsules   = n_inp_capsules
+        self.dim_inp_capsules = dim_inp_capsules
+        self.n_out_capsules   = n_out_capsules
+        self.dim_out_capsules = dim_out_capsules
+        self.num_routing      = num_routing
+        
+        self.weight = nn.Parameter(torch.Tensor(dim_inp_capsules, dim_out_capsules, n_inp_capsules, n_out_capsules))
+        self.weight.data.uniform_(-0.1,0.1)
+
+                                   
+
+    def forward(self, input):
+
+        b     = Variable(torch.zeros(input.shape[0], 1, self.n_inp_capsules, self. n_out_capsules))
+        u_hat = Variable(torch.Tensor(input.shape[0], self.dim_out_capsules, self.n_inp_capsules, self.n_out_capsules))
+
+        for i in range(self.n_inp_capsules):
+            for j in range(self.n_out_capsules):
+                u_hat[:,:,i,j] = torch.mm(input[:, :, i], self.weight[:,:, i,j])
+
+
+        c = F.softmax(b, dim=2)
+        
+        for cnt in range(self.num_routing):
+            c     = F.softmax(b.clone(), dim=2)
+            v_out = self.squash(torch.sum(u_hat*c, dim=2), dim=1)
+            v     = v_out.view(v_out.shape[0], v_out.shape[1], 1, -1)
+            v     = torch.cat([v] * self.n_inp_capsules, dim=2)
+            b    +=  torch.sum(v * u_hat, dim=1, keepdim=True)
+            
+        return v_out
+
+    @staticmethod
+    def squash(input, dim=2):
+        s_square_norm = torch.sum((input * input), dim=dim, keepdim=True)
+        output  = s_square_norm/(1. + s_square_norm) * input/torch.sqrt(s_square_norm)
+        return output
+
+
+if __name__ == "__main__":
+
+    class capsNet(nn.Module):
+
+        def __init__(self):
+            super(capsNet, self).__init__()
+            self.conv1 = nn.Conv2d(1,256,9)
+            self.caps1 = primaryCapsule(in_channels=256, n_channels=32, dim_vector=8, kernel_size=9, stride=2)
+            self.caps2 = digitCapsule(n_inp_capsules=1152, dim_inp_capsules=8, n_out_capsules=10, dim_out_capsules=16)
+
+        
+        def forward(self, x):
+            x = self.conv1(x)
+            x = F.relu(x)
+            x = self.caps1(x)
+            x = x.permute(0,2,1,3,4).contiguous()
+            x = x.view(x.shape[0], x.shape[1],-1)
+            x = self.caps2(x)
+            return x
+
+
+
+    cnet = capsNet()
+    input = torch.randn(5, 1, 28, 28)
+    input = Variable(input)
+    output = cnet(input)
+    print output.shape
---
+++ trainCapsuleOnMnist.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import gzip
+import cPickle as pickle
+import torch.optim as optim
+import itertools
+import copy
+import numpy as np
+from capsules import *
+import matplotlib.pyplot as plt
+
+class capsNet(nn.Module):
+
+    def __init__(self):
+        super(capsNet, self).__init__()
+                                   
+        self.conv1 = nn.Conv2d(1,256,9)
+        self.caps1 = primaryCapsule(in_channels=256, n_channels=32, dim_vector=8, kernel_size=9, stride=2)
+        self.caps2 = digitCapsule(n_inp_capsules=1152, dim_inp_capsules=8, n_out_capsules=10, dim_out_capsules=16)
+
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.caps1(x)
+        x = x.permute(0,2,1,3,4).contiguous()
+        x = x.view(x.shape[0], x.shape[1],-1)
+        x = self.caps2(x)
+        return x
+
+
+class recNet(nn.Module):
+
+    def __init__(self):
+        super(recNet, self).__init__()
+
+        self.fc1  = nn.Linear(16*10, 512)
+        self.fc2  = nn.Linear(512, 1024)
+        self.fc3  = nn.Linear(1024, 784)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], 160)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        x = x.view(x.shape[0], 28, 28)
+        return x
+
+
+class MarginLoss(nn.Module):
+
+    def __init__(self):
+        super(MarginLoss, self).__init__()
+        self.m_plus  = 0.9
+        self.m_minus = 0.1
+        self.lambd   = 0.5
+        
+    def forward(self, input, target):
+        output  = torch.sqrt(torch.sum(input * input, dim=1))
+        out_plus  = square(F.relu(self.m_plus - output))
+        out_minus = square(F.relu(output - self.m_minus))
+        l = torch.mul(target, out_plus) + self.lambd * (1 - target) * out_minus
+        return torch.sum(l)
+    
+
+def square(x):
+    return torch.mul(x,x)
+
+
+class mnist(Dataset):
+
+    def __init__(self, inp, trg, transform=None):
+
+        self.inp       = inp
+        self.trg       = trg
+        self.transform = transform
+        
+    def __len__(self):
+        return self.inp.shape[0]
+
+    def __getitem__(self, idx):
+        img    = self.inp[idx]
+        trg    = self.trg[idx]
+
+                
+        if self.transform:
+            img = self.transform(img)
+            
+        return img, trg
+
+
+    
+
+class ToTensor(object):
+    def __call__(self, image):
+        return torch.from_numpy(image).float()
+
+
+
+if __name__ == "__main__":
+
+    epoch = 100
+    batch_size = 20
+
+    datafile = '/home/guglielmo/dataset/mnist.pkl.gz'
+    with gzip.open(datafile, 'rb') as f:
+        data = pickle.load(f)
+        
+    images = data[0].reshape(data[0].shape[0],1,28,28)
+    labels = data[1]
+
+    n_test_samples  = 4#10000
+    n_valid_samples = 4#10000
+    n_train_samples = 4#50000
+        
+    images_test  = images[:n_test_samples]
+    images_valid = images[n_test_samples:n_test_samples+n_valid_samples]
+    images_train = images[n_test_samples+n_valid_samples:n_test_samples+n_valid_samples+n_train_samples]
+
+    labels_test  = labels[:n_test_samples]
+    labels_valid = labels[n_test_samples:n_test_samples+n_valid_samples]
+    labels_train = labels[n_test_samples+n_valid_samples:n_test_samples+n_valid_samples+n_train_samples]
+    mnistPartTest   = mnist(images_test, labels_test, transform=ToTensor())
+    mnistPartValid  = mnist(images_valid, labels_valid, transform=ToTensor())
+    mnistPartTrain  = mnist(images_train, labels_train, transform=ToTensor())
+
+    testloader  = DataLoader(mnistPartTest,  batch_size=500, shuffle=False, num_workers=1)
+    validloader = DataLoader(mnistPartValid, batch_size=500, shuffle=False, num_workers=1)
+    trainloader = DataLoader(mnistPartTrain, batch_size=batch_size, shuffle=True, num_workers=1)
+
+    cnet = capsNet()
+    rnet = recNet()
+
+    if torch.cuda.is_available():
+        cnet.cuda()
+        rnet.cuda()
+    
+    loss_r    = nn.MSELoss()
+    loss_c    = MarginLoss()
+    
+    optimizer = optim.SGD(itertools.chain(cnet.parameters(), rnet.parameters()), lr=0.001, momentum=0.9)
+
+    n_epoch = 10
+    cnt_epc = 0
+    while cnt_epc < n_epoch:
+        cnt_epc += 1
+        total   = 0
+        correct = 0
+        for input, target in trainloader:
+
+            if torch.cuda.is_available():
+                input = input.cuda()
+        
+            input    = Variable(input)
+            output_c = cnet(input)
+            mask     = torch.zeros(output_c.shape)
+            target_d = torch.zeros((input.shape[0], 10))
+
+            for cnt in range(target.shape[0]):
+                mask[cnt, :, target[cnt]] = 1.
+                target_d[cnt, target[cnt]] = 1.
+
+            
+            if torch.cuda.is_available():
+                target_d, mask  = target_d.cuda(), mask.cuda()
+        
+            target_d = Variable(target_d)
+            mask     = Variable(mask)
+            output_r = rnet(output_c * mask)
+
+            loss_cap = loss_c(output_c, target_d)
+            loss_rec = loss_r(output_r, input)
+            loss_t   = loss_cap + loss_rec
+
+            optimizer.zero_grad()
+            loss_t.backward()
+            optimizer.step()
+
+            _, predicted  = torch.max(torch.sum(output_c * output_c, dim=1), dim=1)
+            correct += torch.sum(predicted.data == target)
+            total   += input.shape[0]
+
+        miss = (1.- float(correct)/float(total)) * 100.
+        print("Epoch %d" % cnt_epc)
+        print("Missclass (Train): %.2f" % miss)
+
+
+        correct = 0
+        total   = 0
+        for input, target in testloader:
+            if torch.cuda.is_available():
+                input = input.cuda()
+            input    = Variable(input)
+            output_c = cnet(input)
+            _, predicted  = torch.max(torch.sum(output_c * output_c, dim=1), dim=1)
+            correct += torch.sum(predicted.data == target)
+            total   += input.shape[0]
+
+        miss = (1.- float(correct)/float(total)) * 100.
+        print("Missclass (Test): %.2f" % miss)
+        
+        mask = torch.zeros(output_c.shape)
+        for cnt in range(target.shape[0]):
+            mask[cnt, :, target[cnt]] = 1.
+        
+        if torch.cuda.is_available():
+            mask  = mask.cuda()
+            
+        mask     = Variable(mask)    
+        output_r = rnet(output_c * mask)
+        
+        _, axes = plt.subplots(2,4)
+        for cnt_r in range(2):
+            if cnt_r == 0:
+                dt = input.data[:,0,:,:]
+            if cnt_r == 1:
+                dt = output_r.data
+                
+            for cnt_c in range(4):
+                axes[cnt_r, cnt_c].imshow(dt[cnt_c])
+
+        plt.show()
